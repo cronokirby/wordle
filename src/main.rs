03f218aafbd94ae655401cd810adf72166768dcd
Lúcás Críostóir Meier <cronokirby@gmail.com>
2022-01-20 20:57:36 +0100

    Define Placement type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+/// This represents the information we receive for each character in a guess.
+///
+/// This information is what we use to then produce our next guess.
+#[derive(Debug)]
+enum Placement {
+    /// The character is at the right spot.
+    Correct,
+    /// The character is at the wrong spot, but in the word nonetheless.
+    Misplaced,
+    /// The character is not in the word.
+    Absent,
+}
+
 fn main() {
     println!("Hello, world!");
 }
